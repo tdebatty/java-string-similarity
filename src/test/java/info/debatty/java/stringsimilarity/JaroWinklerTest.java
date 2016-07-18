988ed03053b67c39d5acf7b3bccb07cbca981bb1
Thibault Debatty <thibault.debatty@gmail.com>
2016-07-18 18:07:57 +0200

    code clean

--- src/test/java/info/debatty/java/stringsimilarity/JaroWinklerTest.java
+++ src/test/java/info/debatty/java/stringsimilarity/JaroWinklerTest.java
@@ -36,25 +36,6 @@ import static org.junit.Assert.*;
  * @author Thibault Debatty
  */
 public class JaroWinklerTest {
-    
-    public JaroWinklerTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
 
     /**

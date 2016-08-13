5f7844ad64041ab31d3be2347c5dc4535313b212
Thibault Debatty <thibault.debatty@gmail.com>
2016-08-13 12:28:20 +0200

    Code clean

--- src/main/java/info/debatty/java/stringsimilarity/CharacterSubstitutionInterface.java
+++ src/main/java/info/debatty/java/stringsimilarity/CharacterSubstitutionInterface.java
@@ -26,14 +26,20 @@ package info.debatty.java.stringsimilarity;
 
 /**
  * Used to indicate the cost of character substitution.
- * 
+ *
  * Cost should always be in [0.0 .. 1.0]
  * For example, in an OCR application, cost('o', 'a') could be 0.4
  * In a checkspelling application, cost('u', 'i') could be 0.4 because these are
  * next to each other on the keyboard...
- * 
+ *
  * @author Thibault Debatty
  */
 public interface CharacterSubstitutionInterface {
-    public double cost(char c1, char c2);
+    /**
+     * Indicate the cost of substitution c1 and c2.
+     * @param c1
+     * @param c2
+     * @return
+     */
+    double cost(char c1, char c2);
 }

289e0828e268d4f4167d02c709769ddcb70fb093
Thibault Debatty <thibault.debatty@gmail.com>
2017-05-31 23:23:52 +0200

    Fix issue #35

--- src/main/java/info/debatty/java/stringsimilarity/Jaccard.java
+++ src/main/java/info/debatty/java/stringsimilarity/Jaccard.java
@@ -91,17 +91,13 @@ public class Jaccard extends ShingleBased implements
         Map<String, Integer> profile1 = getProfile(s1);
         Map<String, Integer> profile2 = getProfile(s2);
 
+
         Set<String> union = new HashSet<String>();
         union.addAll(profile1.keySet());
         union.addAll(profile2.keySet());
 
-        int inter = 0;
-
-        for (String key : union) {
-            if (profile1.containsKey(key) && profile2.containsKey(key)) {
-                inter++;
-            }
-        }
+        int inter = profile1.keySet().size() + profile2.keySet().size()
+                - union.size();
 
         return 1.0 * inter / union.size();
     }
